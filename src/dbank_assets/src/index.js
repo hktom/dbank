3659dd9d39016e826baa4697627f5a627f20df24
tom hikari <tomprescott1565@gmail.com>
2023-12-15 14:51:57 +0100

    dbank

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,5 +1,7 @@
 import { dbank } from "../../declarations/dbank";
 
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
   console.log("window loaded");
+  const currentAmount = await dbank.checkBalance();
+  // document.getElementById("value").innerText = currentAmount;
 });
